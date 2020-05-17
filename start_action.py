1c758101e0a9a18245aa17d9228b5920a0e4a3a6
bin0x00 <sdexca@icloud.com>
2020-05-18 03:23:22 +0530

    Added

---
+++ start_action.py
@@ -0,0 +1,21 @@
+rom pprint import pprint
+import requests
+import json
+import os
+
+# POST /repos/:GITHUB_USERNMAE/:GITHUB_REPO/dispatches
+# Replace with your info...
+
+GITHUB_USERNMAE = "bin0x00"
+GITHUB_REPO = "playing-with-github-actions"
+AUTH_TOKEN = os.environ["GITHUB_REPO_TOKEN"]
+DATA = {}
+
+url = "https://api.github.com/repos/"+GITHUB_USERNMAE+"/"+GITHUB_REPO+"/dispatches"
+headers = {
+    "Authorization": "token "+AUTH_TOKEN,
+    "Accept": "application/vnd.github.everest-preview+json",
+}
+data = {"event_type": "start_action", "client_payload": DATA}
+
+requests.post(url, headers=headers, data=json.dumps(data)).text
